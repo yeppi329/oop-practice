dc15ff994222010391388612716fb57ccdddb811
yeppi329 <yeppi329@naver.com>
2023-08-03 10:37:24 +0900

    첫번째 리팩토링

---
+++ src/test/java/org/example/CalculatorTest.java
@@ -0,0 +1,57 @@
+package org.example;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+public class CalculatorTest {
+
+    /**
+     • 요구사항
+     • 간단한 사칙연산을 할 수 있다.
+     • 양수로만 계산할 수 있다.
+     • 나눗셈에서 0을 나누는 경우 IllegalArgument 예외를 발생시킨다.
+     • MVC패턴(Model-View-Controller) 기반으로 구현한
+     */
+//    @DisplayName("덧셈 연산을 수행한다.")
+//    @Test
+//    void additionTest() {
+//
+//        int result = Calculator.calculate(1, "+", 2);
+//
+//        assertThat(result).isEqualTo(3);
+//    }
+//    @DisplayName("뺄셈 연산을 수행한다.")
+//    @Test
+//    void subtractionTest() {
+//
+//        int result = Calculator.calculate(1, "-", 2);
+//
+//        assertThat(result).isEqualTo(-1);
+//    }
+    @DisplayName("덧셈 연산을 수행한다.")
+    @ParameterizedTest
+    @MethodSource("formulaAndResult")
+    void calculateTest(int operand1,String operator,int operand2,int result) {
+
+        int calculateResult = Calculator.calculate(operand1,operator,operand2);
+
+        assertThat(calculateResult).isEqualTo(result);
+    }
+    private static Stream<Arguments> formulaAndResult(){
+        return Stream.of(
+                arguments(1,"+",2,3),
+                arguments(1,"-",2,-1),
+                arguments(4,"*",2,8),
+                arguments(4,"/",2,2)
+        );
+    }
+}
